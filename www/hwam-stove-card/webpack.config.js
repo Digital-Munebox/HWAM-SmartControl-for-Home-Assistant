f301b4cfce58299116d441ab6fcca5291a731f94
Digital-Munebox <pinaud.adrien@free.fr>
2024-11-27 06:12:02 +0100

    Create webpack.config.js

---
+++ www/hwam-stove-card/webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/cards/hwam-main-card.js',
+  output: {
+    filename: 'hwam-card.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  mode: 'production',
+};
